b6c5ff3d7d6ee5a967a533175c2bb04390a242c7
stalin_laptop <mags961224hjccmt00@gmail.com>
2022-12-16 13:04:33 -0600

    Documented the unencrypted data vulnerability
    All data in the blockchain is public, even though a variable is set as private, that doesn't mean that its value can't be read

--- contracts/2-Unencrypted-Data/Vault.sol
+++ contracts/2-Unencrypted-Data/Vault.sol
@@ -4,7 +4,16 @@ pragma solidity 0.8.9;
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Vault is Ownable {
-  bytes32 private password;
+  // @warning - Even though this is a private variable, be aware that their value can be read from the blockchain
+  // How to read its value? Get the memory location where the data is stored in the smart contract, and just pull it out!
+  
+  // Example of reading the value of this variable using ethersJS
+    // The getStorageAt() function from ethers returns the values that are stored in a contract at an specific slot
+    // Returns the Bytes32 value of the position pos at address addr, as of the blockTag.
+    // Syntax: await ethers.provider.getStorageAt(<address>, <slot>) 
+      //const pwd = await ethers.provider.getStorageAt(this.vault.address, 1);
+
+  bytes32 private password; // Because this is the first variable and it sizes 32 bytes, it will be stored in the 1st slot of the memory's contract
 
   constructor(bytes32 _password) {
     password = _password;
